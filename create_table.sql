bdcdea5f1a0239606691bfc510b55c61eb51237f
Jorge Martínez <113711020+joorgemartinez@users.noreply.github.com>
2025-10-22 21:54:34 +0200

    Añado query create table

---
+++ create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE dbo.source_cars_data 
+(
+  Branch_ID      VARCHAR(200),
+  Dealer_ID      VARCHAR(200),
+  Model_ID       VARCHAR(200),
+  Revenue        BIGINT,
+  Units_Sold     BIGINT,
+  Date_ID        VARCHAR(200),
+  Day            TINYINT,
+  Month          TINYINT,
+  Year           VARCHAR(200),
+  BranchName     VARCHAR(2000),
+  DealerName     VARCHAR(2000),
+  Product_Name   VARCHAR(200)
+);
