ffab6ac7a49bb8312865cd042845a49714686342
Jorge Martínez <113711020+joorgemartinez@users.noreply.github.com>
2025-10-23 19:11:45 +0200

    Arreglo nombre watermark table

--- watermark_table.sql
+++ watermark_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE watermark_tabel 
+CREATE TABLE watermark_table 
 (
 	last_load Varchar(2000)
 )
 
 SELECT min(Date_ID) FROM [dbo].[source_cars_data]
 
-INSERT INTO [dbo].[watermark_tabel]
+INSERT INTO [dbo].[watermark_table]
 VALUES('DT00000') 
